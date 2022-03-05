9f4447c8f4e3e051f70f03681d0492cd4ece9ea5
ItzSphinx <71399083+ItzSphinx@users.noreply.github.com>
2022-03-05 23:09:33 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,14 +1,14 @@
 const info = { body: {
-Name : Sphinx,
-Age : 15,
-Intrested In : ReactJS,
-Learned : 4 languages ,
-languages : ['JS', 'HTML', 'CSS', 'LUA'],
-Librarys : ReactJS, BrainJS,
-Debendcies : ["ExpressJS", "EJS", "DiscordJS"],
-BirthDate : 1-24-2007,
-Discord : Sphinx#1100
-  }
-}
+    Name : "Sphinx",
+    Age : 15,
+    IntrestedIn : "ReactJS",
+    Learned : "4 languages" ,
+    languages : ['JS', 'HTML', 'CSS', 'LUA'],
+    Librarys : ["ReactJS", "BrainJS"],
+    Debendcies : ["ExpressJS", "EJS", "DiscordJS"],
+    BirthDate : 1-24-2007,
+    Discord : "Sphinx | Сфинкс#1100"
+      }
+    }
 
 console.log(info.body)
